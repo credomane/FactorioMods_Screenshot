264df95e60dec0e613d4b330acf5eef5fc252414
Credomane <github@credomane.com>
2016-09-23 00:51:44 -0500

    Don't eat the hotkey event. That's just mean.

--- data.lua
+++ data.lua
@@ -1,20 +1,20 @@
 data:extend({
-  {
-      type = "custom-input",
-      name = "screenshot",
-      key_sequence = "F12",
-      consuming = "all"
-  },
-  {
-  	type = "custom-input",
-  	name = "screenshot-alt",
-  	key_sequence =  "ALT + F12",
-  	consuming = "all"
-  },
-  {
-  	type = "custom-input",
-  	name = "screenshot-config",
-  	key_sequence =  "CONTROL + SHIFT + F12",
-  	consuming = "all"
-  },
+    {
+        type = "custom-input",
+        name = "screenshot",
+        key_sequence = "F12",
+--        consuming = "all"
+    },
+    {
+        type = "custom-input",
+        name = "screenshot-alt",
+        key_sequence =  "ALT + F12",
+--        consuming = "all"
+    },
+    {
+        type = "custom-input",
+        name = "screenshot-config",
+        key_sequence =  "CONTROL + SHIFT + F12",
+--        consuming = "all"
+    },
 })
